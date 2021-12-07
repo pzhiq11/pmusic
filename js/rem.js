9698b675a5a188491556788a321804f67d93cd23
Faith <9909627+pzhiq@user.noreply.gitee.com>
2021-12-07 17:57:14 +0800

    ww

--- js/rem.js
+++ js/rem.js
@@ -1,28 +1,16 @@
 
 function remSize(){
     var deviceWidth = document.documentElement.clientWidth || window.innerWidth;
-    var totalCss = document.getElementById("app");
-    
-
     if(deviceWidth >=750 && deviceWidth <=1000){
         deviceWidth = 750
-        totalCss.style.marginLeft=0+'px';
-        document.documentElement.style.fontSize = (deviceWidth/7.5)+'px';
     }
     if(deviceWidth>=1000){
-        deviceWidth = 1080
-        totalCss.style.marginLeft=600+'px';
-        document.documentElement.style.fontSize = (deviceWidth/15)+'px';
+        deviceWidth = 1024
     }
     if(deviceWidth<=320){
         deviceWidth = 320
-        document.documentElement.style.fontSize = (deviceWidth/7.5)+'px';
     }
-    if(deviceWidth>=320 && deviceWidth <=750 ){
-        totalCss.style.marginLeft=0+'px';
-        document.documentElement.style.fontSize = (deviceWidth/7.5)+'px';
-    }
-    
+    document.documentElement.style.fontSize = (deviceWidth/7.5)+'px';
     document.querySelector('body').style.fontSize = 0.3 +'rem'
     //设计稿是750px
     // 设置一半的宽度
