2772e9603a68c20441d03f00748de57b93c29828
pzhiq <2261727945@qq.com>
2023-03-24 15:24:44 +0800

    Revert "引入自己事件和数据管理包"
    This reverts commit 9a7aa678126137f94823ac50db4212c7e5a5ae0a.

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,56 @@
 <template>
   <div class="marginleft">
-    <router-view class="appH"/> 
+    <router-view class="appH" />
     <playController v-if="$store.state.playController"></playController>
-    
   </div>
-  
 </template>
 <script>
- 
-import playController from './components/playController.vue'
-import esm from "@pzhiq/esm"
+import playController from "./components/playController.vue";
+import esm from "@pzhiq/esm";
 export default {
-  components:{
+  components: {
     playController,
   },
-  mounted(){
-    esm.event.on('test',()=>{
+  mounted() {
+    esm.event.on("test", () => {
       console.log("收到事件啦");
-    })
-    esm.store.on('mainData',(e)=>{
-      console.log("监听到值变化",e);
-    })
+    });
+    esm.store.on("mainData", (e) => {
+      console.log("监听到值变化", e);
+    });
     setTimeout(() => {
       esm.event.emit("test");
-      esm.store.update("mainData",{level:5})
+      esm.store.update("mainData", { level: 5 });
     }, 2000);
-    if(localStorage.userData ==null && localStorage.userData ==undefined){
-      return   
+    if (localStorage.userData == null && localStorage.userData == undefined) {
+      return;
     }
     let userData = JSON.parse(localStorage.userData);
     // console.log(userData)
-    
-    this.$store.commit('setUser',userData);
 
+    this.$store.commit("setUser", userData);
   },
-  methods:{
-    
-      
-   
-    }
-  }
-
+  methods: {},
+};
 </script>
 <style lang="less">
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: '微软雅黑';
-  
+  font-family: "微软雅黑";
 }
-.icon{
+.icon {
   width: 0.3rem;
   height: 0.3rem;
 }
-a{
+a {
   color: #333;
   text-decoration: none;
 }
-.appH{
+.appH {
   height: calc(100vh - 1.2rem);
   overflow: scroll;
-  
-  
 }
 // .marginleft{
 //   margin-left: 5.75rem;
